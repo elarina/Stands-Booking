1df128bdc322861dc27ad5ebd027add44ed378b2
Elena Larina <Elena.Larina@advalange.com>
2024-08-31 05:01:11 +0400

    Spring Hello World

---
+++ src/main/java/com/advalange/StandsStateApplication.java
@@ -0,0 +1,13 @@
+package com.advalange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandsStateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StandsStateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/advalange/stands_state/StandsStateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advalange.stands_state;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StandsStateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
