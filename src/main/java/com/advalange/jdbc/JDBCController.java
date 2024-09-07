41c79572bd96ba1a3c6c5d6efbb4ab3a5c51bd21
Elena Larina <Elena.Larina@advalange.com>
2024-09-08 01:18:15 +0400

    исправлена проблема с часто отваливающейся базой данных

--- src/main/java/com/advalange/jdbc/JDBCController.java
+++ src/main/java/com/advalange/jdbc/JDBCController.java
@@ -2,43 +2,21 @@ package com.advalange.jdbc;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import javax.sql.DataSource;
-
-import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.advalange.model.Employee;
 import com.advalange.model.Stand;
 
 public class JDBCController {
-
-	private JdbcTemplate jdbcTemplate;
 	private DataSource dataSource;
 
 	public JDBCController(DataSource dataSource) {
 		this.dataSource = dataSource;
-		this.jdbcTemplate = new JdbcTemplate(dataSource);
-	}
-
-	public List<Employee> queryTowns() {
-		List<Employee> employees = new ArrayList<Employee>();
-
-		String sql = "SELECT id, name, lastname FROM employees";
-
-		List<Map<String, Object>> records = jdbcTemplate.queryForList(sql);
-
-		for (Map<String, Object> record : records) {
-			Long id = (Long)record.get("id");
-			String name = (String) record.get("name");
-			String lastname = (String) record.get("lastname");
-			employees.add(new Employee(id, name, lastname));
-		}
-
-		return employees;
 	}
 
 	public List<Stand> queryStands() {
@@ -46,68 +24,181 @@ public class JDBCController {
 
 		String sql = "SELECT id, name, ip, username, password, busy FROM stands;";
 
-		List<Map<String, Object>> records = jdbcTemplate.queryForList(sql);
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet rs = null;
+		try {
+			connection = dataSource.getConnection();
+			statement = connection.prepareStatement(sql);
+			rs = statement.executeQuery();
+			while (rs.next()) {
+				int id = rs.getInt(1);
+				String name = rs.getString(2);
+				String ip = rs.getString(3);
+				String username = rs.getString(4);
+				String password = rs.getString(5);
+				boolean busy = rs.getBoolean(6);
+				stands.add(new Stand(id, name, ip, username, password, busy));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 
-		for (Map<String, Object> record : records) {
-			int id = (int) record.get("id");
-			String name = (String) record.get("name");
-			String ip = (String) record.get("ip");
-			String username = (String) record.get("username");
-			String password = (String) record.get("password");
-			boolean busy = (boolean) record.get("busy");
-			stands.add(new Stand(id, name, ip, username, password, busy));
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 		}
 
 		return stands;
 	}
-	
+
 	public List<Employee> queryEmployees() {
 		List<Employee> employees = new ArrayList<Employee>();
-
 		String sql = "SELECT id, firstname, lastname FROM employees;";
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet rs = null;
+		try {
+			connection = dataSource.getConnection();
+			statement = connection.prepareStatement(sql);
+			rs = statement.executeQuery();
+			while (rs.next()) {
+				int id = rs.getInt(1);
+				String name = rs.getString(2);
+				String lastname = rs.getString(3);
+				employees.add(new Employee(id, name, lastname));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 
-		List<Map<String, Object>> records = jdbcTemplate.queryForList(sql);
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 
-		for (Map<String, Object> record : records) {
-			int id = (int) record.get("id");
-			String name = (String) record.get("firstname");
-			String lastname = (String) record.get("lastname");
-			employees.add(new Employee(id, name, lastname));
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 		}
+//		List<Map<String, Object>> records = jdbcTemplate.queryForList(sql);
+//
+//		for (Map<String, Object> record : records) {
+//			int id = (int) record.get("id");
+//			String name = (String) record.get("firstname");
+//			String lastname = (String) record.get("lastname");
+//			employees.add(new Employee(id, name, lastname));
+//		}
 
 		return employees;
 	}
 
-
 	public void removeStand(int id) {
 		String sql = "DELETE FROM stands where id = ?;";
+		PreparedStatement statement = null;
+		Connection connection = null;
 		try {
-			Connection connection = dataSource.getConnection();
-			PreparedStatement statement = connection.prepareStatement(sql);
+			connection = dataSource.getConnection();
+			statement = connection.prepareStatement(sql);
 			statement.setInt(1, id);
 			statement.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+			if (statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 	}
-	
+
 	public void removeEmployee(int id) {
 		String sql = "DELETE FROM employees where id = ?;";
+		PreparedStatement statement = null;
+		Connection connection = null;
 		try {
-			Connection connection = dataSource.getConnection();
-			PreparedStatement statement = connection.prepareStatement(sql);
+			connection = dataSource.getConnection();
+			statement = connection.prepareStatement(sql);
 			statement.setInt(1, id);
 			statement.executeUpdate();
+
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+			if (statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 
 	public void addStand(Stand stand) {
-		Connection connection;
+		PreparedStatement statement = null;
+		Connection connection = null;
 		try {
 			connection = dataSource.getConnection();
-			PreparedStatement statement = connection.prepareStatement(
+			statement = connection.prepareStatement(
 					"INSERT INTO stands (name, ip, username, password, busy) values (?, ?, ?, ?, ?);");
 			statement.setString(1, stand.getName());
 			statement.setString(2, stand.getIp());
@@ -116,24 +207,57 @@ public class JDBCController {
 			statement.setBoolean(5, stand.getBusy());
 			statement.executeUpdate();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} finally {
+			if (statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 		}
 
 	}
-	
+
 	public void addEmployee(Employee employee) {
-		Connection connection;
+		PreparedStatement statement = null;
+		Connection connection = null;
 		try {
 			connection = dataSource.getConnection();
-			PreparedStatement statement = connection.prepareStatement(
-					"INSERT INTO employees (firstname, lastname) values (?, ?);");
+			statement = connection.prepareStatement("INSERT INTO employees (firstname, lastname) values (?, ?);");
 			statement.setString(1, employee.getName());
 			statement.setString(2, employee.getLastname());
 			statement.executeUpdate();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} finally {
+			if (statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
 		}
 
 	}
