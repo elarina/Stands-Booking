9a2ccc76e0719e91eec073d22c66768d8dd87157
Elena Larina <elena.larina@advalange.com>
2025-07-26 16:33:46 +0400

    Добавление поясняющего голубого квадратика "группы" на странице бронирования по группам.

--- src/main/java/com/larina/controllers/BookingController.java
+++ src/main/java/com/larina/controllers/BookingController.java
@@ -30,34 +30,28 @@ public class BookingController {
 	}
 
 	@GetMapping("/booking")
-	public String openBooking(Model model) {
-		Object parentGroupId = model.getAttribute("groupId");
-		List<BookingStandButton> bookingEntities = standsRepository.findAll().stream()
-				.filter(sr -> parentGroupId == null ? true : sr.getGroup().getId().equals(parentGroupId))
-				.map((Stand s) -> new BookingStandButton(s.getName(), s.getId(), "/bookingStand")).toList();
-		model.addAttribute("buttons", bookingEntities);
-		return "booking";
-	}
-
-	@GetMapping("/bookingGroups")
-	public String openBookingGroups(@RequestParam(value = "groupId", required = false) Long parentGroupId,
+	public String openBooking(@RequestParam(value = "groupId", required = false) Long parentGroupId,
+								@RequestParam(required = true) String source,
 			Model model) {
+		
+		
 		List<BookingStandButton> bookingEntities = standsRepository.findAll().stream()
-				.filter(sr -> parentGroupId == null ? false : sr.getGroup().getId().equals(parentGroupId))
+				.filter(sr -> parentGroupId == null ? source.contains("stands") : sr.getGroup().getId().equals(parentGroupId))
 				.map((Stand s) -> new BookingStandButton(s.getName(), s.getId(), "/bookingStand")).toList();
 
 		List<StandsGroupRelation> sgrs = standGroupRelationRepository.findAll().stream()
-				.filter(sgr -> parentGroupId == null ? true : sgr.getParentGroupId().equals(parentGroupId)).toList();
+				.filter(sgr -> parentGroupId == null ? source.contains("groups")  : sgr.getParentGroupId().equals(parentGroupId)).toList();
 
 		List<StandsGroup> rootGroups = standGroupRepository.findAll().stream()
 				.filter(sg -> !sgrs.stream().map(sgr -> sgr.getChildGroupId()).toList().contains(sg.getId())).toList();
 
 		List<BookingStandGroup> groups = standGroupRepository.findAll().stream()
-				.filter(sr -> parentGroupId == null ? rootGroups.contains(sr)
+				.filter(sr -> source.contains("groups") && parentGroupId == null ? rootGroups.contains(sr)
 						: sgrs.stream().anyMatch(sgr -> sgr.getChildGroupId().equals(sr.getId())))
-				.map((StandsGroup sg) -> new BookingStandGroup(sg.getName(), sg.getId(), "/bookingGroups?groupId="))
+				.map((StandsGroup sg) -> new BookingStandGroup(sg.getName(), sg.getId(), "/booking?source='groups'&groupId="))
 				.toList();
 
+		model.addAttribute("source", source);
 		model.addAttribute("groups", groups);
 		model.addAttribute("buttons", bookingEntities);
 		return "/booking";
